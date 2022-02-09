e91db6428a8584e3f900c8e7abf230fad3e15194
Gisela <gespinoza.ugb@gmail.com>
2022-02-09 15:07:37 -0600

    Proyectos

---
+++ componentsvuejs/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+      <list-products title="List Products"/>
+      <!--cart-items :cartItems="cartItems" :itemCount="itemCount"/-->
+    
+  </div>
+</template>
+
+<script>
+import ListProducts from './components/ListProducts.vue'
+//import CartItems from './components/ListProducts.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListProducts,
+    //CartItems
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
